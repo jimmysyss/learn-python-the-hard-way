44efa65405af42b2b5f0fe7aebb8661abbf6fe05
jimmy <jimmysyssie@hotmail.com>
2016-02-24 15:33:08 +0800

    Exercise1

---
+++ src/ex1.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Hello Again")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print("Yay! Printing")
+print("I's much rather you 'not'.")
+print('I "said" do not touch this')
